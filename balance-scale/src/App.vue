39ccb697500a084d05f61bd14de58556c6892620
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-02-22 01:19:05 +0530

    Created Routes for login signup

--- balance-scale/src/App.vue
+++ balance-scale/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div>
-   
-  </div>
-  
+<template>`
+    <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/SignIn">Sign In</router-link>
+    <router-link to="/SignUp">Sign Up</router-link>
+    </nav>
+  <router-view/>
 </template>
 
---
+++ balance-scale/src/pages/SignUp.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Sign Up</h1>
+  </div>
+</template>
+
