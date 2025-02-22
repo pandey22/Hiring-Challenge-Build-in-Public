6346a1e9ffbd54d1a9a41b626de05bf74cff71a1
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-02-22 09:15:44 +0530

    Created Folders for Frontend and Backend

---
+++ frontend/balance-scale/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <nav>
+    <router-link to="/" class="mr-4 text-2xl">Home</router-link>
+    <router-link to="/SignIn" class="mr-4 text-2xl">Sign In</router-link>
+    <router-link to="/SignUp" class="text-2xl">Sign Up</router-link>
+    </nav>
+  <router-view/>
+</template>
---
+++ frontend/balance-scale/src/pages/SignIn.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Sign In</h1>
+  </div>
+</template>
+
+
